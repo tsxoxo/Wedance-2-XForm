0aa117f59ffc60226236562b10cf4c85430e3789
Tom Szwaja <59713582+tsxoxo@users.noreply.github.com>
2025-02-05 13:35:05 +0000

    Add tests

--- src/components/XForm.test.js
+++ src/components/XForm.test.js
@@ -31,5 +31,46 @@ test("3. XForm renders `<form>` with the same amount of child elements as length
         },
     })
     // Count children of <form> element
-    expect(wrapper.text()).toContain("bar")
+    //  expect(wrapper.findAll('???')).toHaveLength(xxx)
+})
+
+test("3. XForm renders given myObject1, myFields1 into myResult1", () => {
+    const myObject1 = {
+        name: "Alex",
+        dateOfBirth: "06.01.1989",
+        eyeColor: "blue",
+    }
+    const myFields1 = [
+        {
+            field: "name",
+            component: "input",
+            type: "text",
+            placeholder: "Name",
+        },
+        { field: "dateOfBirth", component: "input", type: "datetime" },
+        { field: "eyeColor" },
+    ]
+    const myResult1 = `<form>
+        <input name="name" placeholder="Name"....>
+        <input name="dateOfBirth" type="datetime"....>
+        <input name="eyeColor">...
+      </form>`
+
+    const wrapper = mount(XForm, {
+        props: {
+            "v-model": myObject1,
+            fields: myFields1,
+        },
+    })
+    const generatedInputs = wrapper.findAll("input")
+    // Criterion 3: form has same amount of children as there are elements in fields
+    expect(generatedInputs).toHaveLength(3)
+    // Test attribute binding
+    expect(generatedInputs[0]).attributes("name").toBe("name")
+    expect(generatedInputs[0]).attributes("placeholder").toBe("Name")
+    expect(generatedInputs[1]).attributes("name").toBe("dateOfBirth")
+    expect(generatedInputs[1]).attributes("type").toBe("datetime")
+    expect(generatedInputs[2]).attributes("name").toBe("eyeColor")
+    // Test binding change to input[property] => change to myObject[property]
+    // HOw to do that?
 })
