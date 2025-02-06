01f61f4a71b8edddfe19b459eef44bfdc178a2f7
Tom Szwaja <59713582+tsxoxo@users.noreply.github.com>
2025-02-06 09:56:14 +0000

    start writing out implementation strategy

--- src/components/XForm.vue
+++ src/components/XForm.vue
@@ -1,11 +1,20 @@
 <template>
   <form>
-    <!-- todo: add code -->
+    <component v-for='component in components' :is='component.tag' v-bind='attributes' />
   </form>
 </template>
 
-<script>
-export default {
-  // todo: add code
-};
+<script setup>
+const props = defineProps({
+  fields: Object
+})
+// Create internal state. initialize with myObject properties
+// mb clone myObject?
+// bind each of these state values to an input
+// watch each of these and emit onChange
+// mutate state in App.vue
+// possibly look at other XArray solutions for implementation details
+
+// fields question: split attributes and tag into distinct variables?
+const fie
 </script>
