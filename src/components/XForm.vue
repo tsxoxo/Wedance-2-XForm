b3a040f1179ef72fc72d69193d5639af55e78b52
Tom Szwaja <59713582+tsxoxo@users.noreply.github.com>
2025-02-06 11:22:11 +0000

    Add basic layout; attribute bindings; styling

--- src/components/XForm.vue
+++ src/components/XForm.vue
@@ -1,13 +1,28 @@
 <template>
   <form>
-    <component v-for='component in components' :is='component.tag' v-bind='attributes' />
+    <template v-for='item in props.fields'>
+      <div class="form-element">
+        <label for="item.field">{{ item.field }} </label>
+        <component :id="item.field" :value="props.modelValue[item.field]"
+          :is="item.component ? item.component : 'input'" v-bind="omit(item, 'component')" />
+      </div>
+    </template>
+    <!-- <component v-for='component in components' :value="props.modelvalue[component.field]" :is='component.tag' v-bind='attributes' /> -->
   </form>
 </template>
 
 <script setup>
 const props = defineProps({
+  modelValue: Object,
   fields: Object
 })
+const emit = defineEmits([ 'update:modelValue' ])
+const omit = (object, ...property) => {
+  const result = { ...object }
+  property.forEach(property => (delete result[ property ]))
+
+  return result
+}
 // Create internal state. initialize with myObject properties
 // mb clone myObject?
 // bind each of these state values to an input
@@ -16,5 +31,11 @@ const props = defineProps({
 // possibly look at other XArray solutions for implementation details
 
 // fields question: split attributes and tag into distinct variables?
-const fie
 </script>
+<style lang="css" scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+</style>
