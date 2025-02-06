887192d55a8f3d16316c660983d4c1bffe9b112c
Tom Szwaja <59713582+tsxoxo@users.noreply.github.com>
2025-02-06 14:13:59 +0000

    Tidy up comments

--- src/components/XForm.vue
+++ src/components/XForm.vue
@@ -9,8 +9,6 @@
       />
     </template>
   </form>
-
-  <!-- <component v-for='component in components' :value="props.modelvalue[component.field]" :is='component.tag' v-bind='attributes' /> -->
 </template>
 
 <script setup>
@@ -31,15 +29,8 @@ const omit = (object, ...properties) => {
 const emitUpdate = (key, newValue) => {
   emit('update:modelValue', { ...props.modelValue, [ key ]: newValue })
 }
-// Create internal state. initialize with myObject properties
-// mb clone myObject?
-// bind each of these state values to an input
-// watch each of these and emit onChange
-// mutate state in App.vue
-// possibly look at other XArray solutions for implementation details
-
-// fields question: split attributes and tag into distinct variables?
 </script>
+
 <style lang="css" scoped>
 form {
   display: flex;
